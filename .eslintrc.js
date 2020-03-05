8f519ffb5ff2c3dfe9c5446a9f30af0248c69906
Chris Milson <chris.w.milson@gmail.com>
2020-03-05 13:21:02 +0900

    Add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  ignorePatterns: ['build', 'node_modules'],
+  settings: {
+    react: { version: 'detect' }
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 0
+  }
+}
