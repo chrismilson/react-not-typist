57a5b944b44bf6b8db04c4a0ca9715c74bd4ba78
Chris Milson <chris.w.milson@gmail.com>
2020-03-06 10:05:57 +0900

    Add test for rotation

--- src/hooks/__tests__/useRotate.test.ts
+++ src/hooks/__tests__/useRotate.test.ts
@@ -1,3 +1,14 @@
 import useRotate from '../useRotate'
+import { renderHook } from '@testing-library/react-hooks'
 
-test.todo('Rotates through words.')
+test('Rotates through words.', () => {
+  const array = ['A', 'Bee', { message: 'C' }, 4]
+  const { result } = renderHook(() => useRotate(array, 100))
+
+  for (let i = 0; i < 5; i++) {
+    array.forEach(v => {
+      expect(result.current).toBe(v)
+      jest.advanceTimersToNextTimer()
+    })
+  }
+})
