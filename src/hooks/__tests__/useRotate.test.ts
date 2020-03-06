92667e55a0b950b8d14e7702a8999c08d1065758
Chris Milson <chris.w.milson@gmail.com>
2020-03-06 15:54:04 +0900

    Use fake timers

--- src/hooks/__tests__/useRotate.test.ts
+++ src/hooks/__tests__/useRotate.test.ts
@@ -1,13 +1,16 @@
 import useRotate from '../useRotate'
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks'
+
+jest.useFakeTimers()
 
 test('Rotates through words.', () => {
   const array = ['A', 'Bee', { message: 'C' }, 4]
   const { result } = renderHook(() => useRotate(array, 100))
+  act(() => jest.advanceTimersByTime(1))
 
   array.forEach(v => {
     expect(result.current).toBe(v)
-    jest.advanceTimersToNextTimer()
+    act(() => jest.advanceTimersToNextTimer())
   })
 })
 
@@ -17,15 +20,16 @@ test('Rotates on time.', () => {
   const { result } = renderHook(() => useRotate(array, delay))
 
   expect(result.current).toBe(array[0])
-  jest.advanceTimersByTime(delay)
+  act(() => jest.advanceTimersByTime(delay))
   expect(result.current).toBe(array[1])
 })
 
 test('Returns to beginning', () => {
   const array = ['A', 'B']
   const { result } = renderHook(() => useRotate(array, 100))
+  act(() => jest.advanceTimersByTime(1))
 
   expect(result.current).toBe(array[0])
-  jest.advanceTimersToNextTimer(array.length)
+  act(() => jest.advanceTimersToNextTimer(array.length))
   expect(result.current).toBe(array[0])
 })
