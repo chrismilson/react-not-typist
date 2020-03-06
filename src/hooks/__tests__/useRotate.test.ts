2371d14b8039e4339fa707c6d4a01dca5ded83dc
Chris Milson <chris.w.milson@gmail.com>
2020-03-06 10:18:09 +0900

    Add more tests

--- src/hooks/__tests__/useRotate.test.ts
+++ src/hooks/__tests__/useRotate.test.ts
@@ -5,10 +5,27 @@ test('Rotates through words.', () => {
   const array = ['A', 'Bee', { message: 'C' }, 4]
   const { result } = renderHook(() => useRotate(array, 100))
 
-  for (let i = 0; i < 5; i++) {
-    array.forEach(v => {
-      expect(result.current).toBe(v)
-      jest.advanceTimersToNextTimer()
-    })
-  }
+  array.forEach(v => {
+    expect(result.current).toBe(v)
+    jest.advanceTimersToNextTimer()
+  })
+})
+
+test('Rotates on time.', () => {
+  const delay = 1000
+  const array = ['A', 'B']
+  const { result } = renderHook(() => useRotate(array, delay))
+
+  expect(result.current).toBe(array[0])
+  jest.advanceTimersByTime(delay)
+  expect(result.current).toBe(array[1])
+})
+
+test('Returns to beginning', () => {
+  const array = ['A', 'B']
+  const { result } = renderHook(() => useRotate(array, 100))
+
+  expect(result.current).toBe(array[0])
+  jest.advanceTimersToNextTimer(array.length)
+  expect(result.current).toBe(array[0])
 })
