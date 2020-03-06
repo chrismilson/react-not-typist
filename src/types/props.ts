571da000c7f3d85ee779a85f65729183dc756c5f
Chris Milson <chris.w.milson@gmail.com>
2020-03-06 09:35:54 +0900

    Add documentation to NotTypist props

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,5 +1,8 @@
 export interface NotTypistProps {
+  /** The list of strings to rotate through. */
   readonly words?: string[]
+  /** The time taken for the transition between words to complete. */
   readonly speed?: number
+  /** The time waited between word transitions. */
   readonly delay?: number
 }
