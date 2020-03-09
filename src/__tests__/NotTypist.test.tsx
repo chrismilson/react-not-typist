0744569e0e1b02534c9331ec9407e69539027ebc
Chris Milson <chris.w.milson@gmail.com>
2020-03-09 11:33:52 +0900

    Add more tests

--- src/__tests__/NotTypist.test.tsx
+++ src/__tests__/NotTypist.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import NotTypist from '../NotTypist'
+import { act } from 'react-dom/test-utils'
 
-const testCases = [
+jest.useFakeTimers()
+
+test.each([
   ['Hello World!'],
   [
     'Is this the real life?',
@@ -10,10 +13,25 @@ const testCases = [
     'Caught in a landslide;',
     'No escape from reality.'
   ]
-]
-
-test.each(testCases)('Renders first word supplied', (...words) => {
+])('Renders first word supplied', (...words) => {
   const { getByText } = render(<NotTypist words={words} />)
   const element = getByText(words[0])
+  expect(element).toBeInTheDocument()
+})
+
+test.each([
+  [100, 100],
+  [1000, 100],
+  [100, 1000]
+])('Renders second word after delay', (speed, delay) => {
+  const words = ['first', 'second']
+  const { getByText } = render(
+    <NotTypist words={words} speed={speed} delay={delay} />
+  )
+  // the time taken for the first word to appear
+  act(() => jest.advanceTimersByTime(speed))
+  // the time until the second word should appear
+  act(() => jest.advanceTimersByTime(delay))
+  const element = getByText(words[1])
   expect(element).toBeInTheDocument()
 })
