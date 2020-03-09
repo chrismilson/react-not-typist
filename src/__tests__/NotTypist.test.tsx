9b0352dd2c8ead71a15bc03998c51766943c6ad3
Chris Milson <chris.w.milson@gmail.com>
2020-03-09 11:25:00 +0900

    Add more test cases

--- src/__tests__/NotTypist.test.tsx
+++ src/__tests__/NotTypist.test.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import NotTypist from '../NotTypist'
 
-test('Renders words supplied.', () => {
-  const { getByText } = render(<NotTypist words={['Hello World!']} />)
-  const element = getByText('Hello World!')
+const testCases = [
+  ['Hello World!'],
+  [
+    'Is this the real life?',
+    'Is this just fantasy?',
+    'Caught in a landslide;',
+    'No escape from reality.'
+  ]
+]
+
+test.each(testCases)('Renders first word supplied', (...words) => {
+  const { getByText } = render(<NotTypist words={words} />)
+  const element = getByText(words[0])
   expect(element).toBeInTheDocument()
 })
