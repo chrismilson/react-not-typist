06863d57608138d2a53915305a1bcb74d6a0b6ce
Chris Milson <chris.w.milson@gmail.com>
2020-09-11 15:18:53 +0900

    Remove obsolete tests
    Moving from an array to a prop based carousel means that we no longer
    need to test the rotation timing etc.

--- src/__tests__/NotTypist.test.tsx
+++ src/__tests__/NotTypist.test.tsx
@@ -3,8 +3,6 @@ import { render, Matcher } from '@testing-library/react'
 import NotTypist from '../NotTypist'
 import { act } from 'react-dom/test-utils'
 
-jest.useFakeTimers()
-
 /**
  * Returns a matcher for testing-library that looks at the textcontent of nodes.
  * for example
@@ -43,33 +41,4 @@ const withMarkup = (regexpOrString: string | RegExp): Matcher => {
   }
 }
 
-test.each([
-  ['Hello World!'],
-  [
-    'Is this the real life?',
-    'Is this just fantasy?',
-    'Caught in a landslide;',
-    'No escape from reality.'
-  ]
-])('Renders first word supplied', (...words) => {
-  const { getByText } = render(<NotTypist words={words} />)
-  const element = getByText(withMarkup(words[0]))
-  expect(element).toBeInTheDocument()
-})
-
-test.each([
-  [100, 100],
-  [1000, 100],
-  [100, 1000]
-])('Renders second word after delay', (speed, delay) => {
-  const words = ['first', 'second']
-  const { getByText } = render(
-    <NotTypist words={words} speed={speed} delay={delay} />
-  )
-  // the time taken for the first word to appear
-  act(() => jest.advanceTimersByTime(speed))
-  // the time until the second word should appear
-  act(() => jest.advanceTimersByTime(delay))
-  const element = getByText(withMarkup(words[1]))
-  expect(element).toBeInTheDocument()
-})
+it.todo('changes text in the specified time')
